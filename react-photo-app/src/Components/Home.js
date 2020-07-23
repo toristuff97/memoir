9c0cd9fbb4b0c460948d56e53fa614be6d5667f6
Victoria Jones <66696620+toristuff97@users.noreply.github.com>
2020-07-23 11:49:52 -0400

    installed sass and added more photo cards

--- react-photo-app/src/Components/Home.js
+++ react-photo-app/src/Components/Home.js
@@ -39,6 +39,27 @@ export default class Home extends Component {
                         <p class="caption">Was here yesterday, pretty cool!</p>
                         <button type="button" class="btn btn-link">2 Comments</button>
                         <p class="likes">2 Likes</p>
+                        <button type="button" class="btn btn-link">Bookmark</button>
+                    </div>
+                </div>
+                <div class="card" style={{width: '30vw'}}>
+                    <img class="card-img-top" src="https://images.unsplash.com/photo-1517303650219-83c8b1788c4c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd4c162d27ea317ff8c67255e955e3c8&auto=format&fit=crop&w=2691&q=80" rel="nofollow" alt="Card image cap"/>
+                    <div class="card-body">
+                        <p class="username">dietmangotea</p>
+                        <p class="caption">Was here yesterday, pretty cool!</p>
+                        <button type="button" class="btn btn-link">2 Comments</button>
+                        <p class="likes">2 Likes</p>
+                        <button type="button" class="btn btn-link">Bookmark</button>
+                    </div>
+                </div>
+                <div class="card" style={{width: '30vw'}}>
+                    <img class="card-img-top" src="https://images.unsplash.com/photo-1517303650219-83c8b1788c4c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd4c162d27ea317ff8c67255e955e3c8&auto=format&fit=crop&w=2691&q=80" rel="nofollow" alt="Card image cap"/>
+                    <div class="card-body">
+                        <p class="username">dietmangotea</p>
+                        <p class="caption">Was here yesterday, pretty cool!</p>
+                        <button type="button" class="btn btn-link">2 Comments</button>
+                        <p class="likes">2 Likes</p>
+                        <button type="button" class="btn btn-link">Bookmark</button>
                     </div>
                 </div>
                 </div>
