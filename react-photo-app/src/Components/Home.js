5b3e02050e51dbbaaeb6b414ee7d6814b8732a7b
Victoria Jones <66696620+toristuff97@users.noreply.github.com>
2020-07-23 11:32:16 -0400

    edited nav bar

--- react-photo-app/src/Components/Home.js
+++ react-photo-app/src/Components/Home.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 import reactstrap from 'reactstrap';
 
+
 export default class Home extends Component {
     render() {
         return (
             <div>
+                <nav class="navbar navbar-expand-lg">
+                <div class="container">
+                    <a class="navbar-brand" href="javascript:;">Photos</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-bar navbar-kebab"></span>
+                    <span class="navbar-toggler-bar navbar-kebab"></span>
+                    <span class="navbar-toggler-bar navbar-kebab"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                        <a class="nav-link" href="javascript:;">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="javascript:;">Profile</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="javascript:;">Bookmarks</a>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+                </nav>
+
+
                 
             </div>
         )
