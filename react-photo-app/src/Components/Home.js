f4a8ee564a6633822c158aa8766e805e80a6a2ab
Victoria Jones <66696620+toristuff97@users.noreply.github.com>
2020-07-23 10:43:54 -0400

    installed UI kit

---
+++ react-photo-app/src/Components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import reactstrap from 'reactstrap';
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
